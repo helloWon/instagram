ec415c8c4440bae50ddce5bdb32c4a233321410b
helloWon <leeseongwon13@gmail.com>
2022-06-22 02:29:32 +0900

    Feat: Join & Greet

---
+++ instagram/src/main/java/clone/instagram/web/login/LoginController.java
@@ -0,0 +1,88 @@
+package clone.instagram.web.login;
+
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletResponse;
+// import javax.validation.Valid;
+
+import org.springframework.stereotype.Controller;
+// import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
+// import clone.instagram.domain.login.LoginService;
+import clone.instagram.domain.member.Member;
+import clone.instagram.domain.member.MemberService;
+import lombok.RequiredArgsConstructor;
+
+@Controller
+@RequestMapping("/user")
+@RequiredArgsConstructor
+public class LoginController {
+
+    // private final LoginService loginService;
+    private final MemberService memberService;
+
+    @GetMapping("/login")
+    public String loginForm(@ModelAttribute("loginForm") LoginForm form) {
+        return "user/login";
+    }
+
+    // @PostMapping("/login")
+    // public String login(@Valid @ModelAttribute LoginForm form, BindingResult
+    // bindingResult,
+    // HttpServletResponse response) {
+    // if (bindingResult.hasErrors()) {
+    // return "user/login";
+    // }
+
+    // Member loginMember = loginService.login(form.getLoginId(),
+    // form.getPassword());
+
+    // if (loginMember == null) {
+    // bindingResult.reject("loginFail", "id 또는 비밀번호 오류");
+    // return "user/login";
+    // }
+
+    // Cookie idCookie = new Cookie("memberId",
+    // String.valueOf(loginMember.getId()));
+    // response.addCookie(idCookie);
+
+    // return "redirect:/";
+    // }
+
+    // @PostMapping("/logout")
+    // public String logout(HttpServletResponse response) {
+    // expireCookie(response);
+    // return "redirect:/";
+    // }
+
+    @GetMapping("/join")
+    public String joinForm(HttpServletResponse response) {
+        return "user/join";
+    }
+
+    @PostMapping("/join")
+    public String join(@ModelAttribute Member member, RedirectAttributes redirectAttributes) {
+        memberService.join(member);
+        redirectAttributes.addAttribute("name", member.getName());
+        // Cookie idCookie = new Cookie("memberId",
+        // String.valueOf(loginMember.getId()));
+        // response.addCookie(idCookie);
+        return "redirect:/user/home";
+    }
+
+    @GetMapping("/home")
+    public String home() {
+        return "home";
+    }
+
+    // private void expireCookie(HttpServletResponse response) {
+    //     Cookie cookie = new Cookie("memberId", null);
+    //     cookie.setMaxAge(0);
+    //     response.addCookie(cookie);
+    // }
+
+}
---
+++ instagram/src/main/java/clone/instagram/web/member/MemberController.java
@@ -0,0 +1,35 @@
+package clone.instagram.web.member;
+
+import javax.validation.Valid;
+import org.springframework.stereotype.Controller;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import clone.instagram.domain.member.Member;
+import clone.instagram.domain.member.MemberRepository;
+import lombok.RequiredArgsConstructor;
+
+@Controller
+@RequiredArgsConstructor
+@RequestMapping("/members")
+public class MemberController {
+
+    private final MemberRepository memberRepository;
+
+    @GetMapping("/add")
+    public String addForm(@ModelAttribute("member") Member member) {
+        return "members/addMemberForm";
+    }
+
+    @PostMapping("/add")
+    public String save(@Valid @ModelAttribute Member member, BindingResult bindingResult) {
+        if (bindingResult.hasErrors()) {
+            return "members/addMemberForm";
+        }
+        memberRepository.save(member);
+        return "redirect:/";
+    }
+}
